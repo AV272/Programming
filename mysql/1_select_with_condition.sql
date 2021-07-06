06e4ab6649ac6d2755746d0d5c302c71504af041
Alexey Andreyanov <54357784+AV272@users.noreply.github.com>
2021-07-06 17:06:28 +0300

    Update 1_select_with_condition.sql

--- mysql/1_select_with_condition.sql
+++ mysql/1_select_with_condition.sql
@@ -3,3 +3,26 @@ FROM CITY
 WHERE
     COUNTRYCODE = 'USA'
     AND POPULATION > 100000;
+
+
+select distinct city from station where city regexp "^[aeiou].*"; # начинается с гласной
+
+select distinct city from station where city regexp ".*[aeiou]$"; # заканчивается гласной
+
+select distinct city from station where city regexp "^[aeiou].*[aeiou]$";
+
+select distinct city from station where city regexp "^[^aeiou].*"; # не начинается на гласную
+
+select distinct city from station where city regexp ".*[^aeiou]$";
+
+select distinct city from station where city regexp "^[^aeiou].*|.*[^aeiou]$"; # не начинаются на гласную или не заканчиваются
+
+
+select name from students where marks > 75 order by right(name,3), id; # сортировка по последним трем символам строки name.
+
+select name from employee where salary > 2000 and months < 10 order by employee_id asc; # два условия через и.
+
+
+
+
+
