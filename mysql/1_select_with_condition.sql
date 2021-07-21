970842b409f3b68b9c1b5e811564bcb2036fd3ef
Alexey Andreyanov <54357784+AV272@users.noreply.github.com>
2021-07-21 16:03:44 +0300

    Update 1_select_with_condition.sql

--- mysql/1_select_with_condition.sql
+++ mysql/1_select_with_condition.sql
@@ -182,9 +182,42 @@ order by w.power desc, wp.age desc;
 
 
 
+# Задание переменных (set @variable=), создание новых столбцов функцией AS
+set @r1=0, @r2=0, @r3=0, @r4=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
+from(
+  select case when Occupation='Doctor' then (@r1:=@r1+1)
+              when Occupation='Professor' then (@r2:=@r2+1)
+              when Occupation='Singer' then (@r3:=@r3+1)
+              when Occupation='Actor' then (@r4:=@r4+1) 
+         end as RowNumber,
+    case when Occupation='Doctor' then Name end as Doctor,
+    case when Occupation='Professor' then Name end as Professor,
+    case when Occupation='Singer' then Name end as Singer,
+    case when Occupation='Actor' then Name end as Actor
+  from OCCUPATIONS
+  order by Name
+    ) temp
+group by RowNumber;
 
 
 
+# нАХОЖДЕНИЕ ПРОСТЫХ ЧИСЕЛ
+SET @potential_prime = 1;
+SET @divisor = 1;
+
+SELECT GROUP_CONCAT(POTENTIAL_PRIME SEPARATOR '&') FROM
+    (SELECT @potential_prime := @potential_prime + 1 AS POTENTIAL_PRIME FROM
+    information_schema.tables t1,
+    information_schema.tables t2
+    LIMIT 1000) list_of_potential_primes
+WHERE NOT EXISTS(
+    SELECT * FROM
+        (SELECT @divisor := @divisor + 1 AS DIVISOR FROM
+        information_schema.tables t4,
+        information_schema.tables t5
+        LIMIT 1000) list_of_divisors
+    WHERE POTENTIAL_PRIME%DIVISOR = 0 AND DIVISOR< POTENTIAL_PRIME);
 
 
 
