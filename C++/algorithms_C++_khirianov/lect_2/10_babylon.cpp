526617ed4de52692736c3743804b3e62a30b5be6
Alexey <andreyanov.alexey.27@gmail.com>
2023-09-04 17:48:32 +0300

    add last practice tasks from lect_2 for algorithms_C

--- C++/algorithms_C++_khirianov/lect_2/10_babylon.cpp
+++ C++/algorithms_C++_khirianov/lect_2/10_babylon.cpp
@@ -1,10 +1,67 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int x;
+    int x, z;
     cin >> x;
+    int it = 0;
+    int N = 100;
 
+    string *y = new string[N];
+
+        
+    while (x >= 60)
+    {
+        z = x%60;
+        
+        for (int i = 0; i < z%10; i++)
+        {
+            y[it] = "v";
+            it += 1;
+        }
+
+        for (int i = 0; i < z/10; i++)
+        {
+            y[it] = "<";
+            it += 1;
+        }
+        
+        y[it] = ".";
+        it +=1;
+            
+        x = x/60;
+        
+    }
+
+    z = x;
+
+    for (int i = 0; i < z%10; i++)
+    {
+        y[it] = "v";
+        it += 1;
+    }
+
+    for (int i = 0; i < z/10; i++)
+    {
+        y[it] = "<";
+        it += 1;
+    }
+    
+    for(int i = 0; i < N / 2; i++) {
+        string tmp = y[i];
+        y[i] = y[N-1-i];
+        y[N-1-i] = tmp;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << y[i]; 
+    }
+
+    delete[] y;
+
+    return 0;
     
 }
--- C++/algorithms_C++_khirianov/lect_2/9_gai.cpp
+++ C++/algorithms_C++_khirianov/lect_2/9_gai.cpp
@@ -1,16 +1,56 @@
 #include <iostream>
+#include <string>
+#include <typeinfo>
 using namespace std;
 
 int main()
 {
-    int n, sum, max =100;
+    int topA = 0;
+    int N = 100;
+    int sum = 0;
+    int speed;
+    string number;
+    string boss("A999AA");
+    char n[3];
 
-    for (int i = 0; i < max; i++)
-    {
-        cin >> n ;
+
+    cin >> speed >> number;
+    while (number.compare(boss)){
+    	topA += 1;
+        if (topA > N)
+        {
+            cout << "Ошибка! Переполнение массива";
+            exit(0);
+        }
+
+        if (speed > 60)
+        {
+            size_t len =  number.copy( n, 3, 1);
+            if (n[0] == n[1]||n[0] == n[2])
+            {
+                if(n[1]==n[2])
+                {
+                    sum += 1000;
+                }else
+                {
+                    sum += 500;
+                }
+            }else if (n[1]==n[2])
+            {
+                sum += 500;
+            }else
+            {
+                sum += 100;
+            }
+        }
+        
+        // cout << typeid(n).name() << '\n';
+        
+    	cin >> speed >> number;
     }
-    
 
+    cout << sum << '\n';
 
+    return 0;
 
 }
